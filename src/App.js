fe99f9677f0cd6bc06902b7c31491addf01f414b
Aakash Mukherjee <110044436+blackcode1996@users.noreply.github.com>
2023-01-17 12:43:44 +0530

    template

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { Footer } from './Components/Footer';
+import { Navbar } from './Components/Navbar';
+import { MainRoutes } from './Pages/MainRoutes';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <MainRoutes/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/MainRoutes.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { ReqAuth } from '../Components/ReqAuth'
+import { Cart } from './Cart'
+import { Checkout } from './Checkout'
+import { HomePage } from './HomePage'
+import { Login } from './Login'
+import { ProductList } from './ProductList'
+import { SingleProduct } from './SingleProduct'
+
+export const MainRoutes = () => {
+  return (
+    <Routes>
+        <Route path='/' element={<HomePage/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/products" element={<ProductList/>}></Route>
+        <Route path="/products:id" 
+        element={
+        <ReqAuth>
+            <SingleProduct/>
+        </ReqAuth>}>
+        </Route>
+        <Route path="/checkout" 
+        element={
+        <ReqAuth>
+            <Checkout/>
+        </ReqAuth>}>
+        </Route>
+        <Route path="/cart" 
+        element={
+        <ReqAuth>
+            <Cart/>
+        </ReqAuth>}>
+        </Route>
+        <Route path="*">{<div>Page Not Found</div>}</Route>
+    </Routes>
+  )
+}
