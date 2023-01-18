eed8f987470810b5184bd37bdd3365ba8243d91d
Sajan Kumar <sajan99559@gmail.com>
2023-01-18 10:10:55 +0530

    day-2

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -20,7 +20,14 @@ const HomePage = () => {
                      {id:4,img:"https://assets.tatacliq.com/medias/sys_master/images/45581158940702.jpg"},
                      {id:5,img:"https://assets.tatacliq.com/medias/sys_master/images/45581159006238.jpg"},
                      {id:6,img:"https://assets.tatacliq.com/medias/sys_master/images/45536787398686.jpg"},
-                   ]
+                   ];
+      const last=[{id:1,img:"https://assets.tatacliq.com/medias/sys_master/images/45513137127454.jpg"},
+                   {id:2,img:"https://assets.tatacliq.com/medias/sys_master/images/45567584534558.jpg"},
+                   {id:3,img:"https://assets.tatacliq.com/medias/sys_master/images/45513137389598.jpg"},
+                   {id:4,img:"https://assets.tatacliq.com/medias/sys_master/images/45513137455134.jpg"},
+                   {id:5,img:"https://assets.tatacliq.com/medias/sys_master/images/45513137258526.jpg"},
+                   {id:6,img:"https://assets.tatacliq.com/medias/sys_master/images/45513137061918.jpg"}
+                 ]             
   return (
     <div  style={{background:"rgb(236,236,236)"}}>
         <div style={{ width: "100%"}}>
@@ -29,7 +36,7 @@ const HomePage = () => {
             <div key={item.id}>
               <img className="slide1" src={item.img} alt=""  />
               
-            </div> 
+            </div>    
           ))}
         </Slider>
       </div>
@@ -100,15 +107,86 @@ const HomePage = () => {
             <img src="https://assets.tatacliq.com/medias/sys_master/images/45435992703006.jpg" alt="" />
             <img src="https://assets.tatacliq.com/medias/sys_master/images/45382587318302.jpg" alt="" />
           </div>
+          <div style={{marginBottom:"65px"}}>
+            <img src="https://assets.tatacliq.com/medias/sys_master/images/45589087453214.gif" alt="" />
+          </div>
+          <div className='banner'>
+            <img src="https://assets.tatacliq.com/medias/sys_master/images/45032248639518.jpg" alt="" style={{height:"300px"}}/>
+          </div>
+          <div>
+            <img src="https://tata-cliq-clone.netlify.app/Images/1.png" alt="" />
+          </div>
+          <div className='card'>
+            <img src="https://assets.tatacliq.com/medias/sys_master/images/45584580378654.jpg" alt="" />
+            <img src="https://assets.tatacliq.com/medias/sys_master/images/45584586997790.jpg" alt="" />
+            <img src="https://assets.tatacliq.com/medias/sys_master/images/45584587063326.jpg" alt="" />
+            <img src="https://assets.tatacliq.com/medias/sys_master/images/45584587259934.jpg" alt="" />
+          </div>
+          <div className='card'>
+            <img src="https://assets.tatacliq.com/medias/sys_master/images/45584587653150.jpg" alt="" />
+            <img src="https://assets.tatacliq.com/medias/sys_master/images/45584587784222.jpg" alt="" />
+            <img src="https://assets.tatacliq.com/medias/sys_master/images/45584587718686.jpg" alt="" />
+            <img src="https://assets.tatacliq.com/medias/sys_master/images/45584587522078.jpg" alt="" />
+            
+          </div>
           <div>
             <img src="https://assets.tatacliq.com/medias/sys_master/images/45589087453214.gif" alt="" />
           </div>
-          <div>
-            <img src="" alt="" />
+          <div className='card'>
+            <img src="https://assets.tatacliq.com/medias/sys_master/images/45584575987742.jpg" alt="" />
+            <img src="https://assets.tatacliq.com/medias/sys_master/images/45584576053278.jpg" alt="" />
+            <img src="https://assets.tatacliq.com/medias/sys_master/images/45584576118814.jpg" alt="" />
+            <img src="https://assets.tatacliq.com/medias/sys_master/images/45584576184350.jpg" alt="" />
           </div>
-        <div>
-          
+          <div className='card'>
+          <img src="https://assets.tatacliq.com/medias/sys_master/images/45584576446494.jpg" alt="" />
+            <img src="https://assets.tatacliq.com/medias/sys_master/images/45584576512030.jpg" alt="" />
+            <img src="https://assets.tatacliq.com/medias/sys_master/images/45554447581214.jpg" alt="" />
+            <img src="https://assets.tatacliq.com/medias/sys_master/images/45584576708638.jpg" alt="" />
+          </div>
+          <div>
+            <img src="https://tata-cliq-clone.netlify.app/Images/5.png" alt="" />
+          </div>
+          <div className='card' >
+            <img src="https://assets.tatacliq.com/medias/sys_master/images/45435991523358.jpg" alt="" />
+            <img src="https://assets.tatacliq.com/medias/sys_master/images/45435991588894.jpg" alt="" />
+            <img src="https://assets.tatacliq.com/medias/sys_master/images/45567584862238.jpg" alt="" />
+            <img src="https://assets.tatacliq.com/medias/sys_master/images/45435991719966.jpg" alt="" />
+          </div>
+          <div className="banner" >
+          <img src="https://assets.tatacliq.com/medias/sys_master/images/45536787398686.jpg" alt="" style={{marginTop:"40px"}}/>
         </div>
+        <div className='child-card'>
+          <img src="https://assets.tatacliq.com/medias/sys_master/images/45581162905630.jpg" alt="" />
+          <img src="https://assets.tatacliq.com/medias/sys_master/images/45581162971166.jpg" alt="" />
+        </div>
+        <div  className='card'>
+          <img src="https://assets.tatacliq.com/medias/sys_master/images/45581163036702.jpg" alt="" />
+          <img src="https://assets.tatacliq.com/medias/sys_master/images/45581163102238.jpg" alt="" />
+          <img src="https://assets.tatacliq.com/medias/sys_master/images/45581163167774.jpg" alt="" />
+          <img src="https://assets.tatacliq.com/medias/sys_master/images/45581163233310.jpg" alt="" />
+        </div>
+        <div>
+          <img src="https://tata-cliq-clone.netlify.app/Images/3.png" alt="" />
+        </div>
+        <div style={{ width: "100%"}}>
+        <Slider {...settings1}>
+          {last.map((item) => (
+            <div key={item.id}>
+              <img className="slide1" src={item.img} alt=""  />           
+            </div> 
+          ))}
+        </Slider>
+      </div>
+      <div>
+        <img src="https://assets.tatacliq.com/medias/sys_master/images/45461314043934.jpg" alt="" />
+      </div>
+      <div className='card'>
+        <img src="https://assets.tatacliq.com/medias/sys_master/images/45576216608798.jpg" alt="" />
+        <img src="https://assets.tatacliq.com/medias/sys_master/images/45576216674334.jpg" alt="" />
+        <img src="https://assets.tatacliq.com/medias/sys_master/images/45576216739870.jpg" alt="" />
+        <img src="https://assets.tatacliq.com/medias/sys_master/images/45576216805406.jpg" alt="" />
+      </div>
       </div>
     </div>
   )
