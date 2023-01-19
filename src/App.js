0371e4fe721b15e82439c6ad46139617521a2b2a
Shivam00002 <dubey02shivam@gmail.com>
2023-01-20 00:25:40 +0530

    day-3

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@
 import { Footer } from './Components/Footer';
 import { Navbar } from './Components/Navbar';
 import { MainRoutes } from './Pages/MainRoutes';
+import { SingleProduct } from './Pages/SingleProduct';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <MainRoutes/>
-      <Footer/>
+      {/* <Navbar/> */}
+      <SingleProduct/>
+      {/* <MainRoutes/> */}
+      {/* <Footer/> */}
     </div>
   );
 }
