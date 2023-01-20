03db374e2d200c99d99491f8c7c377eac11d7a32
aniketparmar29 <aniketparmar068@gmail.com>
2023-01-20 12:30:50 +0530

    ASD

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import { useState } from "react";
+import { Box, Button, Flex, FormControl, FormLabel, Select, Stack } from "@chakra-ui/react";
 
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
-}
+  const [selectedFilters, setSelectedFilters] = useState([]);
 
-export default Sidebar
+  return (
+    <Box>
+      <FormControl>
+        <FormLabel>Filters</FormLabel>
+        <Stack>
+          <Button
+            variant={selectedFilters.includes("red") ? "solid" : "outline"}
+            onClick={() => {
+              if (selectedFilters.includes("red")) {
+                setSelectedFilters(selectedFilters.filter(f => f !== "red"));
+              } else {
+                setSelectedFilters([...selectedFilters, "red"]);
+              }
+            }}
+          >
+            Red
+          </Button>
+          <Button
+            variant={selectedFilters.includes("green") ? "solid" : "outline"}
+            onClick={() => {
+              if (selectedFilters.includes("green")) {
+                setSelectedFilters(selectedFilters.filter(f => f !== "green"));
+              } else {
+                setSelectedFilters([...selectedFilters, "green"]);
+              }
+            }}
+          >
+            Green
+          </Button>
+          <Button
+            variant={selectedFilters.includes("blue") ? "solid" : "outline"}
+            onClick={() => {
+              if (selectedFilters.includes("blue")) {
+                setSelectedFilters(selectedFilters.filter(f => f !== "blue"));
+              } else {
+                setSelectedFilters([...selectedFilters, "blue"]);
+              }
+            }}
+          >
+            Blue
+          </Button>
+        </Stack>
+      </FormControl>
+    </Box>
+  );
+};
+
+export default Sidebar;
--- src/Pages/ProductListPage.jsx
+++ src/Pages/ProductListPage.jsx
@@ -1,17 +1,37 @@
 import React from 'react'
 import ProductList from '../Components/ProductList'
 import Sidebar from '../Components/Sidebar'
+import styled from 'styled-components'
 import {Navbar} from '../Components/Navbar'
 import {Footer} from '../Components/Footer'
-
 export const ProductListPage = () => {
   return (
-    <div>
-        <Navbar/>
-        <Sidebar/>
-        <ProductList/>
-        <Footer/>
+    <>
+    <Navbar/>
+    <div style={{backgroundColor:"#ececec"}}>
+      <DivWrapper>
+        <FilterWrapper>
+          <Sidebar/>
+        </FilterWrapper>
+        <BooksListWrapper>
+          <ProductList/>
+        </BooksListWrapper>
+      </DivWrapper>
     </div>
+    <Footer/>
+    </>
   )
 }
 
+const DivWrapper=styled.div`
+display:flex;
+gap:20px;
+`
+const FilterWrapper=styled.div`
+width: 20%;
+margin-left: 30px;
+margin-top: 100px;
+`
+const BooksListWrapper=styled.div`
+width: 80%;
+`
