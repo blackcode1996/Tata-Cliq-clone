300daa40cea54bab397146220d851b7ef8ab1d62
sranbir392 <sranbir392@hotmail.com>
2023-01-22 02:16:36 +0530

    f2

--- src/Pages/MainRoutes.jsx
+++ src/Pages/MainRoutes.jsx
@@ -11,45 +11,20 @@ import { SingleProduct } from "./SingleProduct";
 import { Admin } from "../Admin/Pages/Admin";
 import Pagenotfound from "./Pagenotfound";
 import EditUser from "../Admin/Components/Users/EditUser";
-import { Wishlist } from "./Wishlist";
+import EditProduct from "../Admin/Components/Product/EditProduct";
 export const MainRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
       <Route path="/login" element={<Login />}></Route>
       <Route path="/products" element={<ProductListPage />}></Route>
-      <Route path="/products:id" element={<SingleProduct />}></Route>
+      <Route path="/products/:id" element={<SingleProduct />}></Route>
       <Route path="/checkout" element={<Checkout />}></Route>
       <Route path="/cart" element={<Cart />}></Route>
       <Route path="*" element={<Pagenotfound />}></Route>
       <Route path="/admin" element={<Admin />}></Route>
       <Route path="/admin/edit/:id" element={<EditUser />}></Route>
-      <Route path="/Wishlist" element={<Wishlist />}></Route>
-
-import { Cart } from './Cart'
-import { Checkout } from './Checkout'
-import  HomePage  from './HomePage'
-import { Login } from './Login'
-import { ProductListPage } from './ProductListPage'
-import { SingleProduct } from './SingleProduct'
-import {Admin} from '../Admin/Pages/Admin'
-import Pagenotfound from './Pagenotfound'
-import EditUser from '../Admin/Components/Users/EditUser'
-import EditProduct from '../Admin/Components/Product/EditProduct';
-export const MainRoutes = () => {
-  return (
-    <Routes>
-        <Route path='/' element={<HomePage/>}></Route>
-        <Route path="/login" element={<Login/>}></Route>
-        <Route path="/products" element={<ProductListPage/>}></Route>
-        <Route path="/products/:id" element={<SingleProduct/>}></Route>
-        <Route path="/checkout"  element={<Checkout/>}></Route>
-        <Route path="/cart"  element={<Cart/>}></Route>
-        <Route path="*" element={<Pagenotfound/>}></Route>
-        <Route path='/admin' element={<Admin/>}></Route>
-        <Route path="/admin/edit/:id" element={<EditUser />}></Route>
-        <Route path="/admin/upadte/:id" element={<EditProduct />}></Route>
-
+      <Route path="/admin/upadte/:id" element={<EditProduct />}></Route>
     </Routes>
   );
 };
