49bce33a6dac89d6b331e7ccc56e701b703f5e8b
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2023-08-14 23:25:08 +0530

    Update store.js

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -7,7 +7,9 @@ import { reducer as AuthReducer } from "./AuthReducer/reducer";
 import { reducer as ProductReducer } from "./ProductReducer/reducer";
 import thunk from "redux-thunk";
 
-const rootReducer = combineReducers({ AuthReducer, ProductReducer });
+const rootReducer =
+
+ combineReducers({ AuthReducer, ProductReducer });
 
 const store = legacy_createStore(rootReducer,applyMiddleware(thunk));
 
